f083f6b14abf9cbad05fb90b9ac25dfff4489c04
ShivamKumar2k16 <shivamkumar2k16k@gmail.com>
2024-09-13 09:25:18 +0530

    will remove after tedst

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,5 @@ import { AppComponent } from './app/app.component';
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
 
-  
+  //will remove this after test
 //<!-- https://www.youtube.com/watch?v=QjaNv7s1gW0  check this video for the project -->
